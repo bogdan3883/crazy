2ff42f8974e81fcdf9243c3e761e5cdd03c54b85
Bogdan <93480175+bogdan3883@users.noreply.github.com>
2022-08-16 12:16:29 +0300

    112

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-const button = document.querySelector('button');
-
-const user = 'Admin';
-const password = 'TheMaster';
-
-let userInput = prompt('Login');
-
-if (userInput == "") {
-    alert('Canceled');
-}   else if (userInput != user) {
-    alert("I don't know you");
-} else if (userInput == user) {
-  let  userPassword = prompt('Password:');
-    if (userPassword == password) {
-        alert('Welcome!');
-    } else if (userPassword != password) {
-        alert('Canceled');
-    } else if (userPassword == ""){
-        alert('Wrong password!');
-    }
+function morning() {
+    alert("Good morning!");
 }
 
+function afternoon() {
+    alert("Good afternoon!");
+}
 
+function evening() {
+    alert("Good evening!");
+}
 
-
-// function greet() {
-//     const time = prompt('What time is it?');
-    
-//     if (time < 12) {
-//         alert('Good morning!')
-//     } else if (time >= 12 && time <= 18){
-//         alert('Good afternoon!')
-//     } else {
-//         alert('Good evening!')
-//     }
-// }
-
-// button.addEventListener('click', greet);
-
-// if (!age < 14 && age > 90 )
+function night() {
+    alert("Good night!");
+}
